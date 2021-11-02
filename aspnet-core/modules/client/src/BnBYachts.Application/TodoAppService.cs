54cbc685790000a06d8f731f935b329445b0ef23
umar draz <u.draz@gems.techverx.com>
2021-11-02 18:16:56 +0500

    fix auth using idv 4

---
+++ aspnet-core/modules/client/src/BnBYachts.Application/TodoAppService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace BnBYachts
+{
+    [Authorize]
+    public class TodoAppService : ApplicationService, ITodoAppService
+    {
+        
+
+        // TODO: Implement the methods here...
+        public async Task<List<string>> GetListAsync()
+        {
+            await Task.CompletedTask;
+            return new List<string>{"umar"};
+
+        }
+    }
+}
