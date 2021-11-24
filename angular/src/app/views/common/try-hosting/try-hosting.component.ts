645741aa31e1124c8c72a7f5a2cd06dc1f225778
hananafzal88 <hananafzal88@gmail.com>
2021-11-24 20:19:20 +0500

    BB-58 done.

---
+++ angular/src/app/views/common/try-hosting/try-hosting.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-try-hosting',
+  templateUrl: './try-hosting.component.html',
+  styleUrls: ['./try-hosting.component.scss']
+})
+export class TryHostingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
