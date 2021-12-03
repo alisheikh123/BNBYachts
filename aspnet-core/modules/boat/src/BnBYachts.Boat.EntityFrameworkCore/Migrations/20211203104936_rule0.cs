dcaf7e72e42d76f6abb92659ad512b69cae2fb23
FarhanAhmed845 <farhanahmed555.pakistan@gmail.com>
2021-12-03 04:19:34 -0700

    changes..

---
+++ aspnet-core/modules/boat/src/BnBYachts.Boat.EntityFrameworkCore/Migrations/20211203104936_rule0.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BnBYachts.Boat.Migrations
+{
+    public partial class rule0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSafetyOption",
+                table: "Rules");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSafetyOption",
+                table: "Rules",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
