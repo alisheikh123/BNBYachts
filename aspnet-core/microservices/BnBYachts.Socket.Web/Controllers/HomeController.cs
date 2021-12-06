9f27ce0884db7280e2d0289756ac29c36f5110d0
umar draz <u.draz@gems.techverx.com>
2021-12-06 18:33:59 +0500

    add the web socket for push notifications

---
+++ aspnet-core/microservices/BnBYachts.Socket.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace BnBYachts.Socket.Web.Controllers
+{
+    public class HomeController : AbpController
+    {
+        public ActionResult Index()
+        {
+            return Json("Application is running");
+        }
+    }
+}
