94608fbc1234ac33b12f2e0d4c67714e7629ff63
umar draz <u.draz@gems.techverx.com>
2021-12-14 16:47:29 +0500

    add the base seeder

--- aspnet-core/buildingblocks/seeder/BnByachts.Seeder/Seeders/BaseSeeder.cs
+++ aspnet-core/buildingblocks/seeder/BnByachts.Seeder/Seeders/BaseSeeder.cs
@@ -1,19 +1,15 @@
 ﻿using BnBYachts.EventBusShared;
 using System.IO;
 using System.Reflection;
+using Volo.Abp.DependencyInjection;
 
 
 namespace BnByachts.Seeder.Seeders
 {
     public abstract class BaseSeeder
     {
-        protected readonly EventBusDispatcher _eventBusDispatcher;
-
+        public IAbpLazyServiceProvider LazyServiceProvider { get; set; }
+        protected EventBusDispatcher _eventBusDispatcher => LazyServiceProvider.LazyGetRequiredService<EventBusDispatcher>();
         protected static string GetFullPath => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, @"Data/");
-
-        protected BaseSeeder(EventBusDispatcher eventBusDispatcher)
-        {
-            _eventBusDispatcher = eventBusDispatcher;
-        }
     }
 }
