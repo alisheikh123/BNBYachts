73b31e14cdc61287aafa1754df6be4085f3909fd
hananafzal88 <49321535+hananafzal88@users.noreply.github.com>
2021-12-15 14:18:33 +0500

    interface updated

--- aspnet-core/modules/boat/src/BnBYachts.Boat.Application.Contracts/Interfaces/Event/IEventAppService.cs
+++ aspnet-core/modules/boat/src/BnBYachts.Boat.Application.Contracts/Interfaces/Event/IEventAppService.cs
@@ -3,10 +3,11 @@ using BnBYachts.Boat.Event.Requestable;
 using BnBYachts.Boat.Event.Transferables;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
 
 namespace BnBYachts.Boat.Interfaces.Boat
 {
-    public interface IEventAppService
+    public interface IEventAppService : IApplicationService
     {
         Task<ICollection<BoatLookupTransferable>> GetBoats();
         Task<bool> SaveEvent(EventRequestable boatEvent);
