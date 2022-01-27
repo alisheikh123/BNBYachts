3470d9a0fc989f751c6f80f66ec1c71a7cabda55
hananafzal88 <49321535+hananafzal88@users.noreply.github.com>
2022-01-27 12:14:55 +0500

    Update BoatApplicationModule.cs

--- aspnet-core/modules/boat/src/BnBYachts.Boat.Application/BoatApplicationModule.cs
+++ aspnet-core/modules/boat/src/BnBYachts.Boat.Application/BoatApplicationModule.cs
@@ -1,4 +1,5 @@
-﻿using Volo.Abp.Account;
+﻿using System;
+using Volo.Abp.Account;
 using Volo.Abp.AutoMapper;
 using Volo.Abp.FeatureManagement;
 using Volo.Abp.Identity;
@@ -6,6 +7,7 @@ using Volo.Abp.Modularity;
 using Volo.Abp.PermissionManagement;
 using Volo.Abp.SettingManagement;
 using Volo.Abp.TenantManagement;
+using Volo.Abp.Timing;
 
 namespace BnBYachts.Boat
 {
@@ -26,6 +28,10 @@ namespace BnBYachts.Boat
             Configure<AbpAutoMapperOptions>(options =>
             {
                 options.AddMaps<BoatApplicationModule>();
+            });
+            Configure<AbpClockOptions>(options =>
+            {
+                options.Kind = DateTimeKind.Local;
             });
         }
     }
