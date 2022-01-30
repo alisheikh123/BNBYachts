3b5cbdbe37a8b2c315a6e8bbcda60fe853822ad1
Asadullah213 <asadullah.ehsan@techverx.com>
2022-01-30 21:22:51 +0500

    Carousel slider added in try-hosting page.

--- angular/src/app/views/common/try-hosting/try-hosting.component.ts
+++ angular/src/app/views/common/try-hosting/try-hosting.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Carousel } from 'primeng/carousel';
 
 @Component({
   selector: 'app-try-hosting',
   templateUrl: './try-hosting.component.html',
-  styleUrls: ['./try-hosting.component.scss']
 })
 export class TryHostingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-    
+  boats: any[];
+  responsive: any[];
+  page: number;
+  constructor() {    
+    this.responsive = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 1
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
   }
 
+  ngOnInit(): void {
+    this.boats = [
+      '../../../../assets/images/card-1.png',
+      '../../../../assets/images/card-1.png',
+      '../../../../assets/images/card-1.png',
+      '../../../../assets/images/card-1.png',
+      '../../../../assets/images/corporate.png',
+      '../../../../assets/images/card-3.png',
+      '../../../../assets/images/card-3.png'
+    ];
+  }
 }
