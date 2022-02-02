bdbadc0b74b14e79c32f472aa9fb40391a7f7625
umar draz <u.draz@gems.techverx.com>
2022-02-02 17:15:42 +0500

    remove health checks

--- aspnet-core/microservices/BnBYachts.Boat.HttpApi.Host/Startup.cs
+++ aspnet-core/microservices/BnBYachts.Boat.HttpApi.Host/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace BnBYachts.Boat
 {
@@ -15,47 +10,13 @@ namespace BnBYachts.Boat
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHealthChecks();
             services.AddApplication<BoatHttpApiHostModule>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseHealthChecks("/health", new HealthCheckOptions
-            {
-                ResponseWriter = async (context, report) =>
-                {
-                    context.Response.ContentType = "application/json";
-
-                    var response = new HealthCheckResponse
-                    {
-                        Status = report.Status.ToString(),
-                        Checks = report.Entries.Select(x => new HealthCheck
-                        {
-                            Component = x.Key,
-                            Status = x.Value.Status.ToString(),
-                            Description = x.Value.Description
-                        }),
-                        Duration = report.TotalDuration
-                    };
-                    await context.Response.WriteAsync(JsonConvert.SerializeObject(response)).ConfigureAwait(false);
-                }
-            });
             app.UseCookiePolicy(new CookiePolicyOptions { MinimumSameSitePolicy = SameSiteMode.Strict });
             app.InitializeApplication();
         }
-    }
-
-    public class HealthCheck
-    {
-        public string Status { get; set; }
-        public string Component { get; set; }
-        public string Description { get; set; }
-    }
-    public class HealthCheckResponse
-    {
-        public string Status { get; set; }
-        public IEnumerable<HealthCheck> Checks { get; set; }
-        public TimeSpan Duration { get; set; }
     }
 }
