7a478d7d8f8ed7d4d316c019ca03a78627472c13
umar draz <u.draz@gems.techverx.com>
2022-02-02 15:56:02 +0500

    add health check

--- aspnet-core/microservices/BnBYachts.Boat.HttpApi.Host/Startup.cs
+++ aspnet-core/microservices/BnBYachts.Boat.HttpApi.Host/Startup.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace BnBYachts.Boat
 {
@@ -16,8 +20,41 @@ namespace BnBYachts.Boat
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    context.Response.ContentType = "application/json";
+
+                    var response = new HealthCheckResponse
+                    {
+                        Status = report.Status.ToString(),
+                        Checks = report.Entries.Select(x => new HealthCheck
+                        {
+                            Component = x.Key,
+                            Status = x.Value.Status.ToString(),
+                            Description = x.Value.Description
+                        }),
+                        Duration = report.TotalDuration
+                    };
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(response)).ConfigureAwait(false);
+                }
+            });
             app.UseCookiePolicy(new CookiePolicyOptions { MinimumSameSitePolicy = SameSiteMode.Strict });
             app.InitializeApplication();
         }
+    }
+
+    public class HealthCheck
+    {
+        public string Status { get; set; }
+        public string Component { get; set; }
+        public string Description { get; set; }
+    }
+    public class HealthCheckResponse
+    {
+        public string Status { get; set; }
+        public IEnumerable<HealthCheck> Checks { get; set; }
+        public TimeSpan Duration { get; set; }
     }
 }
