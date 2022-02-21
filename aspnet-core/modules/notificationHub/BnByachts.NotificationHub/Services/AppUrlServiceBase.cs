e9d2653a3c04340a762eff557bf5cb5915563280
umar draz <umar.draz@techverx.com>
2022-02-21 20:07:22 +0500

    add the email template

---
+++ aspnet-core/modules/notificationHub/BnByachts.NotificationHub/Services/AppUrlServiceBase.cs
@@ -0,0 +1,28 @@
+﻿using BnBYachts.Shared.Interface;
+using BnBYachts.Shared.Model;
+using Microsoft.Extensions.Options;
+
+
+namespace BnByachts.NotificationHub.Services
+{
+    public class AppUrlServiceBase : IAppUrlService
+    {
+
+        private readonly BaseURLModel _baseURLModel;
+
+        public AppUrlServiceBase(IOptions<BaseURLModel> baseURLModel)
+        {
+            _baseURLModel = baseURLModel.Value;
+        }
+        public string CreatePasswordResetUrlFormat(int? tenantId)
+        {
+            return CreatePasswordResetUrlFormat("");
+        }
+        public string CreatePasswordResetUrlFormat(string tenancyName)
+        {
+            var resetLink = _baseURLModel.ClientUrl + "/" + _baseURLModel.PasswordResetRoute + "?userId={userId}&resetCode={resetCode}";
+            return resetLink;
+        }
+
+    }
+}
---
+++ aspnet-core/modules/notificationHub/BnByachts.NotificationHub/Services/UserEmailer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using BnByachts.NotificationHub.Templates;
+
+namespace BnByachts.NotificationHub.Services
+{
+    public class UserEmailer : IUserEmailer
+    {
+        private readonly IEmailTemplateProvider _emailTemplateProvider;
+        private readonly IMailer _mailer;
+        // used for styling action links on email messages.
+        private string _emailButtonStyle =
+            "padding-left: 30px; padding-right: 30px; padding-top: 12px; padding-bottom: 12px; color: #ffffff; background-color: #00bb77; font-size: 14pt; text-decoration: none;";
+        private string _emailButtonColor = "#00bb77";
+        public UserEmailer(IEmailTemplateProvider emailTemplateProvider,
+            IMailer mailer)
+        {
+            _emailTemplateProvider = emailTemplateProvider;
+            _mailer = mailer;
+        }
+        public async Task SendPasswordResetLinkAsync(string link = null, string logUrl = null)
+        {
+           
+            var emailTemplate = GetTitleAndSubTitle(1, "BNB password reset.", "This email is sent you to reset and re-create your password.", logUrl);
+            var mailMessage = new StringBuilder();
+            mailMessage.AppendLine("<b>" + "Name surname" + "</b>: " + "Ali" + "<br />");
+            mailMessage.AppendLine("<b>" + "User name" + "</b>: " + "Az," + "<br />");
+            if (!link.IsNullOrEmpty())
+            {
+                link = link.Replace("{userId}", "1");
+                link = link.Replace("{resetCode}", Uri.EscapeDataString("Code"));
+                link = EncryptQueryParameters(link);
+                mailMessage.AppendLine("<br />");
+                mailMessage.AppendLine("Please click the link below to reset your password:" + "<br /><br />");
+                mailMessage.AppendLine("<a style=\"" + _emailButtonStyle + "\" bg-color=\"" + _emailButtonColor + "\" href=\"" + link + "\">" + "Reset" + "</a>");
+                mailMessage.AppendLine("<br />");
+                mailMessage.AppendLine("<br />");
+                mailMessage.AppendLine("<br />");
+                mailMessage.AppendLine("<span style=\"font-size: 9pt;\">" + "If the button above doesn't work, paste this into your browser:" + "</span><br />");
+                mailMessage.AppendLine("<span style=\"font-size: 8pt;\">" + link + "</span>");
+            }
+            await ReplaceBodyAndSend("email", "BNB password reset", emailTemplate, mailMessage);
+        }
+        private StringBuilder GetTitleAndSubTitle(int? tenantId, string title, string subTitle, string logUrl)
+        {
+            var emailTemplate = new StringBuilder(_emailTemplateProvider.GetDefaultTemplate(tenantId, logUrl));
+            emailTemplate.Replace("{EMAIL_TITLE}", title);
+            emailTemplate.Replace("{EMAIL_SUB_TITLE}", subTitle);
+
+            return emailTemplate;
+        }
+        private string EncryptQueryParameters(string link, string encrptedParameterName = "c")
+        {
+            if (!link.Contains("?"))
+            {
+                return link;
+            }
+            var basePath = link.Substring(0, link.IndexOf('?'));
+            var query = link.Substring(link.IndexOf('?')).TrimStart('?');
+            return basePath + "?" + encrptedParameterName + "=" + HttpUtility.UrlEncode(SimpleStringCipher.Instance.Encrypt(query));
+        }
+        private async Task ReplaceBodyAndSend(string emailAddress, string subject, StringBuilder emailTemplate, StringBuilder mailMessage)
+        {
+            emailTemplate.Replace("{EMAIL_BODY}", mailMessage.ToString());
+            await _mailer.SendEmailAsync
+                (
+                 emailAddress, subject, emailTemplate.ToString(),
+                  true
+                );
+        }
+    }
+}
