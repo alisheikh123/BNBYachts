eab409b5ee5f73c30a84666c6e5cb06f1a0bf2d0
umar draz <umar.draz@techverx.com>
2022-03-07 19:10:11 +0500

    fix env issue

--- angular/src/env.js
+++ angular/src/env.js
@@ -1,22 +1,20 @@
-(function (window) {
-    window.__env = window.__env || {};
-    // API url
-      window.__env.BOAT_API_URL= 'https://localhost:44387',
-      window.__env.BOOKING_API_URL= 'https://localhost:44337',
-      window.__env.CORE_API_URL= 'https://localhost:44347',
-      window.__env.IDENTITY_API_URL= 'https://localhost:44311',
-      window.__env.PAYMENTS_API_URL= 'https://localhost:44366',
-      window.__env.CHAT_API_URL='https://localhost:44363',
-      window.__env.S3BUCKET_URL='https://bnbyachts.s3.amazonaws.com',
-      window.__env.CAPTAIN_APP_URL='http://localhost:4201/',
-      window.__env.MANAGEMENT_APP_URL='http://localhost:4202/',
-      window.__env.CLEANING_APP_URL='http://localhost:4203/',
-      window.__env.stripeKey= "pk_test_51JjjR4IQmeuKTcwEUxVurdeswUrX0kjd0thsgPIYZpOiuPm7wf2XdKjWBjU2FtsT8PGjxmj7lCXU7QPA35qXuRPY00YGIKhP5f",
-      window.__env.identity_authority= "https://localhost:44311",
-      window.__env.identity_redirectUrl= "http://localhost:4200",
-      window.__env.identity_postLogoutRedirectUri= "http://localhost:4200",
-      window.__env.identity_clientId= "BnBYachts_App",
-      window.__env.identity_scope= "openid Core Booking Boat Payments HostGateway Chat",
-      window.__env.identity_responseType= 'code'
-      window.__env.enableDebug = false;
-  }(this));
+window._env = {
+    BOAT_API_URL:'https://localhost:44387',
+    BOOKING_API_URL: 'https://localhost:44337',
+    CORE_API_URL: 'https://localhost:44347',
+    IDENTITY_API_URL: 'https://localhost:44311',
+    PAYMENTS_API_URL: 'https://localhost:44366',
+    CHAT_API_URL:'https://localhost:44363',
+    S3BUCKET_URL:'https://bnbyachts.s3.amazonaws.com',
+    CAPTAIN_APP_URL:'http://localhost:4201/',
+    MANAGEMENT_APP_URL:'http://localhost:4202/',
+    CLEANING_APP_URL:'http://localhost:4203/',
+    stripeKey: "pk_test_51JjjR4IQmeuKTcwEUxVurdeswUrX0kjd0thsgPIYZpOiuPm7wf2XdKjWBjU2FtsT8PGjxmj7lCXU7QPA35qXuRPY00YGIKhP5f",
+    identity_authority: "https://localhost:44311",
+    identity_redirectUrl: "http://localhost:4200",
+    identity_postLogoutRedirectUri: "http://localhost:4200",
+    identity_clientId: "BnBYachts_App",
+    identity_scope: "openid Core Booking Boat Payments HostGateway Chat",
+    identity_responseType: 'code',
+    enableDebug : false
+};
--- angular/src/environments/environment.stag.ts
+++ angular/src/environments/environment.stag.ts
@@ -1,20 +1,22 @@
 export const environment = {
-  staging: true,
-  production:false,
-  BOAT_API_URL: 'http://boat.bnbstaging.techverxapps.com',
-  BOOKING_API_URL: 'http://booking.bnbstaging.techverxapps.com',
-  CORE_API_URL: 'http://core.bnbstaging.techverxapps.com',
-  IDENTITY_API_URL: 'http://idv.bnbstaging.techverxapps.com',
-  PAYMENTS_API_URL: 'http://payment.bnbstaging.techverxapps.com',
-  S3BUCKET_URL:'https://bnbyachts.s3.amazonaws.com',
-  CHAT_API_URL:'http://chat.bnbstaging.techverxapps.com',
-  stripeKey: "pk_test_51JjjR4IQmeuKTcwEUxVurdeswUrX0kjd0thsgPIYZpOiuPm7wf2XdKjWBjU2FtsT8PGjxmj7lCXU7QPA35qXuRPY00YGIKhP5f",
+  production: false,
+  BOAT_API_URL:(<any>window)._env.BOAT_API_URL,
+  BOOKING_API_URL: (<any>window)._env.BOOKING_API_URL,
+  CORE_API_URL: (<any>window)._env.CORE_API_URL,
+  IDENTITY_API_URL:(<any>window)._env.IDENTITY_API_URL,
+  PAYMENTS_API_URL: (<any>window)._env.PAYMENTS_API_URL,
+  CHAT_API_URL:(<any>window)._env.CHAT_API_URL,
+  S3BUCKET_URL:(<any>window)._env.S3BUCKET_URL,
+  CAPTAIN_APP_URL:(<any>window)._env.CAPTAIN_APP_URL,
+  MANAGEMENT_APP_URL:(<any>window)._env.MANAGEMENT_APP_URL,
+  CLEANING_APP_URL:(<any>window)._env.CLEANING_APP_URL,
+  stripeKey: (<any>window)._env.stripeKey,
   Identity: {
-    authority: "http://idv.bnbstaging.techverxapps.com",
-    redirectUrl: "http://client.bnbstaging.techverxapps.com",
-    postLogoutRedirectUri: "http://client.bnbstaging.techverxapps.com",
-    clientId: "BnBYachts_App",
-    scope: "openid Core Booking Boat Payments Chat",
-    responseType: 'code'
-  }   
+    authority: (<any>window)._env.identity_authority_authority,
+    redirectUrl: (<any>window)._env.identity_redirectUrl,
+    postLogoutRedirectUri: (<any>window)._env.identity_postLogoutRedirectUri,
+    clientId: (<any>window)._env.identity_clientId,
+    scope: (<any>window)._env.identity_scope,
+    responseType:(<any>window)._env.identity_responseType
+  }
 };
