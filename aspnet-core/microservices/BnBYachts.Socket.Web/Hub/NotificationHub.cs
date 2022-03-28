62e6788389657096e66dae5165151f1d6a5072d7
alirazatahir123 <89031022+alirazatahir123@users.noreply.github.com>
2022-03-28 17:21:19 +0500

    vomit

---
+++ aspnet-core/microservices/BnBYachts.Socket.Web/Hub/NotificationHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.SignalR;
+
+namespace BnBYachts.Socket.Web.Hub
+{
+    public class NotificationHub: AbpHub
+    {
+        public async Task SendMessage(string targetUserName, string message)
+        {
+
+            Console.WriteLine($"{targetUserName} {message }");
+            await Task.CompletedTask;
+        }
+    }
+}
