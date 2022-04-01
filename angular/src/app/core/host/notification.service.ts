e40e68df42bfe2819c62c9badfeafd30d884679a
umar draz <umar.draz@techverx.com>
2022-04-01 19:18:39 +0500

    Update notification.service.ts

--- angular/src/app/core/host/notification.service.ts
+++ angular/src/app/core/host/notification.service.ts
@@ -20,12 +20,11 @@ this.notificationGenerated.next(obj);
   constructor(private http: HttpClient,private errorService: ErrorService) { }
 
   getNotificationCount(){
-    return this.http.get(this.notificationsUrl + '/notification-count').pipe(catchError(this.errorService.handleError));
+    return this.http.get(this.notificationsUrl + 'notification-count').pipe(catchError(this.errorService.handleError));
   }
 
   getNotificationMessage() {
-    
-    return this.http.get(environment.NOTIFICATION_APP_URL +'/api/app/in-app-notification').pipe(
+    return this.http.get(environment.NOTIFICATION_APP_URL +'api/app/in-app-notification').pipe(
       catchError(this.errorService.handleError));
   }
 
